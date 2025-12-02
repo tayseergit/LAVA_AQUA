c05a136092297280fc85f6c74ebb948bfa351fa2
tayseergit <eng.tayseermatar@gmail.com>
2025-12-02 23:17:21 +0300

    add astar

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # main.py
 from core.solver import Solver
 from presentation.display_map import *
-level ="maps7.txt"
+level ="maps12.txt"
 if __name__ == "__main__":
     open(r"D:\\مشاريع\\LAVA_AQUA\\data\\states_log.txt", "w", encoding="utf-8").close()
 
